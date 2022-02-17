9d33d7a6a7b0cd4cab333777af523aaa338e2149
Pradeesh <pradeeshprakasan17@gmail.com>
2022-02-17 13:16:13 +0530

    login page

--- Workspace/angularapp/src/app/login/login.component.ts
+++ Workspace/angularapp/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder,Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private formbuilder: FormBuilder) {}
+
+  emailRegex = "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@" 
+            + "[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$";
+  passwordRegex ="(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@#$^!%*?&])[A-Za-z\d$@#$^!%*?&].{7,15}";
+
+  loginForm = this.formbuilder.group({
+    email: ['',[Validators.required,Validators.pattern(this.emailRegex)]],
+    password:['',[Validators.required,Validators.pattern(this.passwordRegex)]],
+  }); 
+
+  onSubmit(){
+    console.log(this.loginForm.value);
+  }
+
+  get email(){
+    return this.loginForm.get('email');
+  }
+  get emailcontrols(){
+    return this.loginForm.controls['email'].errors;
+  }
+
+  get password(){
+    return this.loginForm.get('password');
+  }
+  get passwordcontrols(){
+    return this.loginForm.controls['password'].errors;
+  }
 
   ngOnInit(): void {
   }
-
 }
