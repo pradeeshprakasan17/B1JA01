2fd3a110bc95fef376e95e08f148f854fa5bd62c
Pradeesh <pradeeshprakasan17@gmail.com>
2022-03-03 19:12:30 +0530

    components,angular materials

--- Workspace/angularapp/src/app/app.module.ts
+++ Workspace/angularapp/src/app/app.module.ts
@@ -3,24 +3,38 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule,ReactiveFormsModule} from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { SignupComponent } from './signup/signup.component';
 import { LoginComponent } from './login/login.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { UserNavbarComponent } from './user-navbar/user-navbar.component';
+import { CartComponent } from './cart/cart.component';
+import { OrdersComponent } from './orders/orders.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SignupComponent,
     LoginComponent,
-    NotFoundComponent 
+    NotFoundComponent,
+    HomepageComponent,
+    UserNavbarComponent,
+    CartComponent,
+    OrdersComponent 
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
     ReactiveFormsModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
